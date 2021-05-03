8922fd01b695acb85915eb28ebe64bc9da0c933d
Akash C A <akashca@Akashs-MacBook-Air.local>
2021-05-03 17:28:53 +0530

    Public subnet added

--- ex-2/main.tf
+++ ex-2/main.tf
@@ -2,10 +2,19 @@ provider "aws" {
    region   = "us-east-2"
 }
 resource "aws_vpc" "main" {
-   cider_block = "0.0.0.0/0"
-   instance_tennancy = "default"
-
+   cidr_block = "10.0.0.0/24"
+   instance_tenancy = "default"
+   
    tags = {
       Name = "main"
+   }
+}
+
+resource "aws_subnet" "subnet-p" {
+   vpc_id = "${aws_vpc.main.id}"
+   cidr_block = "10.0.0.0/24"
+     
+   tags = {
+      Name = "subnet-p"
    }
 }
